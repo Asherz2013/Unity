c61d5d99f4fae860a06fe8fe6ea1c2ce69585fe0
Asherz2013 <ashley.shaw2009@gmail.com>
2016-06-10 21:07:43 +0100

    Bowl Master
    Bowling!

---
+++ Bowl Master/Assets/START Pack/Scripts/ball.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ball : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+    private AudioSource audioSource;
+
+    public Vector3 launchVelocity;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.velocity = launchVelocity;
+
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
