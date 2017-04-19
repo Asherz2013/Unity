810b5cee77de72e696543a570d02568b2a092943
Asherz2013 <ashley.shaw2009@gmail.com>
2017-04-19 23:25:31 +0100

    Adding TwinStick to the Repro :)

---
+++ TwinStick/Assets/Game/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class GameManager : MonoBehaviour
+{
+    public bool recording = true;
+
+    private bool isPaused = false;
+    private float initialFixedDelta;
+
+    void Start()
+    {
+        initialFixedDelta = Time.fixedDeltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(CrossPlatformInputManager.GetButton ("Fire1"))
+        {
+            recording = false;
+        }
+        else
+        {
+            recording = true;
+        }
+
+        if (isPaused)
+        {
+            isPaused = false;
+            PauseGame();
+        }
+        else
+        {
+            ResumeGame();
+        }
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0;
+        Time.fixedDeltaTime = 0;
+    }
+
+    void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = initialFixedDelta;
+    }
+
+    void OnApplicationPause(bool pauseStatus)
+    {
+        isPaused = pauseStatus;
+    }
+}
---
+++ TwinStick/Assets/Game/ReplaySystem.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReplaySystem : MonoBehaviour
+{
+
+    private const int bufferFrames = 100;
+    private MyKeyFrame[] keyFrames = new MyKeyFrame[bufferFrames];
+    private Rigidbody rigidBody;
+    private GameManager manager;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        manager = GameObject.FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(manager.recording)
+        {
+            Record();
+        }
+        else
+        {
+            PlayBack();
+        }
+    }
+
+    void Record()
+    {
+        rigidBody.isKinematic = false;
+        int frameNumber = Time.frameCount % bufferFrames;
+        keyFrames[frameNumber] = new MyKeyFrame(Time.time, transform.position, transform.rotation);
+    }
+
+    void PlayBack()
+    {
+        rigidBody.isKinematic = true;
+        int frameNumber = Time.frameCount % bufferFrames;
+        transform.position = keyFrames[frameNumber].position;
+        transform.rotation = keyFrames[frameNumber].rotation;
+    }
+}
+
+/// <summary>
+/// A structure for storing time, rotation and position
+/// </summary>
+public class MyKeyFrame
+{
+    public float frameTime;
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public MyKeyFrame(float time, Vector3 pos, Quaternion rot)
+    {
+        frameTime = time;
+        position = pos;
+        rotation = rot;
+    }
+}
