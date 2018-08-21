ec60d76a39efe40e64eacd33dbc84aa510b985cd
Ashley Shaw <ashley.shaw2009@gmail.com>
2018-08-21 22:48:06 +0100

    Dungeon Escape * Created a "Moss Giant" Enemy * Created all anims for it. * Moved Player Script into its own folder ** Done this so we can quickly find scripts * Created a new "Enemy" Parent Class ** This class has been made Abstract * Created an empty "Spider" class for when this is made.

---
+++ Dungeon Escape/Assets/Assets/Scripts/Enemy/Spider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spider : Enemy
+{
+    public override void Attack()
+    {
+        base.Attack();
+
+        Debug.Log("Spider Attack");
+    }
+
+    public override void Update()
+    {
+        
+    }
+}
