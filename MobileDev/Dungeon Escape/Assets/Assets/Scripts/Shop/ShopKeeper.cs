91cf9bed6730f90f71d1863149243e8ff92ca7c4
Ashley Shaw <ashley.shaw2009@gmail.com>
2018-09-02 22:33:06 +0100

    Dungeon Escape * Made a "Shopkeeper" * Made a "Shop" UI Panel * Made a UIManager Singleton class which holds all the UI information.

---
+++ MobileDev/Dungeon Escape/Assets/Assets/Scripts/Shop/ShopKeeper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopKeeper : MonoBehaviour
+{
+    public GameObject shopPanel;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Player player = other.GetComponent<Player>();
+            if(player)
+            {
+                UIManager.Instance.OpenShop(player._diamondCount);
+            }
+            shopPanel.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            shopPanel.SetActive(false);
+        }
+    }
+}
---
+++ MobileDev/Dungeon Escape/Assets/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    #region Singleton
+    private static UIManager _instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("UI Manager is NULL!");
+            }
+            return _instance;
+        }
+    }
+    #endregion
+
+    public Text playerGemCountText;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void OpenShop(int gemCount)
+    {
+        playerGemCountText.text = gemCount + "G";
+    }
+}
