5ddd57d9fe16bc15b9bdd81450cc7f63b932dcf5
Ashley Shaw <ashley.shaw2009@gmail.com>
2018-09-27 20:51:22 +0100

    Started on "Terminal Hacker" for the 3D course.

---
+++ CCUD3D/TerminalHacker/Assets/Scripts/Hacker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu()
+    {
+        Terminal.ClearScreen();
+        var gretting = "Hello Ash";
+        Terminal.WriteLine(gretting);
+        Terminal.WriteLine("What would you like to hack into?");
+        Terminal.WriteLine("Press 1 for the Local Library");
+        Terminal.WriteLine("Press 2 for the Police Station");
+        Terminal.WriteLine("Enter your selection:");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
