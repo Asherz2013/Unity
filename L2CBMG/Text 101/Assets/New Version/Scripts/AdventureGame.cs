9fc025ba1835c6b1fbbf1e0741cb35381ea202bd
Ashley Shaw <ashley.shaw2009@gmail.com>
2018-09-27 22:12:53 +0100

    Text 101 * Created a new version of the "Adventure Game". * This has started to use ScriptableObjects as the states.

---
+++ L2CBMG/Text 101/Assets/New Version/Scripts/AdventureGame.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField]
+    Text storyText;
+
+    [SerializeField]
+    State startingState;
+
+    State state;
+
+    // Use this for initialization
+    void Start()
+    {
+        state = startingState;
+        storyText.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ L2CBMG/Text 101/Assets/New Version/Scripts/ScriptableObjects/State.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(10,14)]
+    [SerializeField]
+    string storyText;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+}
