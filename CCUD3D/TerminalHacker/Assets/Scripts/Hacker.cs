d7c1a91d76ccd286f648b8d5af4e904a18cfc6fd
Ashley Shaw <ashley.shaw2009@gmail.com>
2018-11-02 22:13:08 +0000

    Terminal * Created a State Machine to hold what part of the game we are in * Added a User Input Handler (OnUserInput) ** Made the State Machine detmine what should be classed as input

--- CCUD3D/TerminalHacker/Assets/Scripts/Hacker.cs
+++ CCUD3D/TerminalHacker/Assets/Scripts/Hacker.cs
@@ -2,6 +2,13 @@
 
 public class Hacker : MonoBehaviour
 {
+    // Game State
+    int level;
+    enum Screen { MainMenu, Password, Win };
+    Screen currentScreen;
+
+    string password;
+
     // Use this for initialization
     void Start()
     {
@@ -10,18 +17,81 @@ public class Hacker : MonoBehaviour
 
     void ShowMainMenu()
     {
+        currentScreen = Screen.MainMenu;
         Terminal.ClearScreen();
-        var gretting = "Hello Ash";
-        Terminal.WriteLine(gretting);
         Terminal.WriteLine("What would you like to hack into?");
         Terminal.WriteLine("Press 1 for the Local Library");
         Terminal.WriteLine("Press 2 for the Police Station");
         Terminal.WriteLine("Enter your selection:");
     }
 
-    // Update is called once per frame
-    void Update()
+    // Message - Holds the users input
+    void OnUserInput(string input)
     {
+        if (input == "menu")
+        {
+            ShowMainMenu();
+        }
+        else
+        {
+            switch (currentScreen)
+            {
+                case Screen.MainMenu:
+                    RunMainMenu(input);
+                    break;
 
+                case Screen.Password:
+                    RunPasswordCracker(input);
+                    break;
+
+                case Screen.Win:
+                    break;
+            }
+        }
+    }
+
+    void RunMainMenu(string input)
+    {
+        // Print will evaluate to a boolean based on expression
+        //print(input == "1");
+        if (input == "007")
+        {
+            Terminal.WriteLine("Please select a level Mr. Bond!");
+        }
+        else if (input == "1")
+        {
+            level = 1;
+            password = "donkey";
+            StartGame();
+        }
+        else if (input == "2")
+        {
+            level = 2;
+            password = "password";
+            StartGame();
+        }
+        else
+        {
+            Terminal.WriteLine("Please choose a valid level.");
+        }
+    }
+
+    void StartGame()
+    {
+        currentScreen = Screen.Password;
+        Terminal.WriteLine("You have choosen " + level);
+        Terminal.WriteLine("Please enter your password: ");
+    }
+
+    void RunPasswordCracker(string input)
+    {
+        if (input == password)
+        {
+            Terminal.WriteLine("Congratulations this is correct.");
+        }
+        else
+        {
+            Terminal.WriteLine("Sorry Wrong Password!");
+        }
     }
 }
