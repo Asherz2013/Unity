2039ec8c7107b4403361b02d54ae5cb1b0943184
Ashley Shaw <ashley.shaw2009@gmail.com>
2018-11-05 12:34:59 +0000

    Boost * Made a "Rocket" Script ** Handles Thursting and Rotating of the ship * Added an Audio Source ** This only plays when the user hits the Thrusters. * Updated the level to have some Obstacles in it.

---
+++ CCUD3D/Boost/Assets/Scripts/Rocket.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody),typeof(AudioSource))]
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thrust();
+        Rotate();
+    }
+
+    private void Thrust()
+    {
+        // Thrusting
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            if (audioSource.isPlaying)
+            {
+                audioSource.Stop();
+            }
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // Take Manual control of Rotation
+
+        // Rotate Left
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        // Rotate Right
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+
+        rigidBody.freezeRotation = false; // Resume Physics control of Rotation
+    }
+}
