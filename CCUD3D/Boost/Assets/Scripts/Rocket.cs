8a893fbc1b5b2df9b883e24ad1296817dba44906
Ashley Shaw <ashley.shaw2009@gmail.com>
2018-11-05 16:17:26 +0000

    Boost * Gave the Rocket some attributes which can be tweeked in the editor ** Namely Thrust and Rotation speeds * Added OnCollisionEnter to move onto the next level * Added another level * Gave all the GameObjects in the level Tags ** Rocket = "Player" ** Launch Pad = "Friendly" ** Landing Pad = "Finish" ** Everything else was left blank to fall through into Default * Added a Backgdrop * Added some new Materials

--- CCUD3D/Boost/Assets/Scripts/Rocket.cs
+++ CCUD3D/Boost/Assets/Scripts/Rocket.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Rigidbody),typeof(AudioSource))]
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
     AudioSource audioSource;
+
+    [SerializeField] float thrustAmount = 50f;
+    [SerializeField] float rotationSpeed = 250f;
 
     // Use this for initialization
     void Start()
@@ -20,12 +24,30 @@ public class Rocket : MonoBehaviour
         Rotate();
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "Friendly":
+                // Do Nothing :)
+                break;
+            case "Finish":
+                // Player has landed on the "Ending Stip"
+                SceneManager.LoadScene(1);
+                break;
+            default:
+                // You WILL BE DEAD
+                SceneManager.LoadScene(0);
+                break;
+        }
+    }
+
     private void Thrust()
     {
         // Thrusting
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * thrustAmount);
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
@@ -47,12 +69,12 @@ public class Rocket : MonoBehaviour
         // Rotate Left
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
         }
         // Rotate Right
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationSpeed * Time.deltaTime);
         }
 
         rigidBody.freezeRotation = false; // Resume Physics control of Rotation
