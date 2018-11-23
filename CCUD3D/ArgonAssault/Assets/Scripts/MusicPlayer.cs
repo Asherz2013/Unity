ebf58f0b39e650dc46c885cf616b50e4cf304faa
Ashley Shaw <ashley.shaw2009@gmail.com>
2018-11-23 23:05:09 +0000

    Argon Assault * Added a Splash Screen level ** This level plays music and transitions to Level 1 after 2 seconds automatically ** Added a Music Script ** Added some music to play * Added a Player Script
    There are more Standard Assets and Ships to add, however at this moment in time I haven't delete the "Other stuff" that bloats the folders....

---
+++ CCUD3D/ArgonAssault/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("LoadFirstScene", 2f);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
